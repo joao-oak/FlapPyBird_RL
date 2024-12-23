127ad697be096d50d1d7d4db879339b37db3f6bc
joao-carvalho <joao.david.17@gmail.com>
2024-12-23 13:00:19 +0000

    PPO

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import asyncio
 from src.flappybird_env import FlappyBirdEnv
 from src import deep_q_n
 from src.flappy import Flappy
+# from src import train
 
 if __name__ == "__main__":
 
     # # To run the game
     # asyncio.run(Flappy().start())
 
-    # To train a model
-    env = FlappyBirdEnv()
-    model = deep_q_n.DQNAgent(env)
-    model.training(5000)
-
-    # # To play the game using a trained model
+    # # To train a model
+    # ## DQN
     # env = FlappyBirdEnv()
     # model = deep_q_n.DQNAgent(env)
-    # model.ai_play('models/TopModel/50.pt')
+    # model.training(5000)
+
+    # ##PPO
+    # train()
+
+    # To play the game using a trained model
+    env = FlappyBirdEnv()
+    model = deep_q_n.DQNAgent(env)
+    model.ai_play('models/TopModel/50.pt')
