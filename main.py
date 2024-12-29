2325573375dc55b325576504a70d2d5393c05c4d
Bruno Silva <bmfs.96@gmail.com>
2024-12-29 12:20:59 +0000

    ppo trainings - done

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
                         print(f"actor:{lr_actor}, critic:{lr_critic}, update_timestep:{update_timestep}")
                         env = FlappyBirdEnv(fps=fps)
                         ppo_agent = ppo.PPO(env, lr_actor=lr_actor, lr_critic=lr_critic, update_timestep=update_timestep, gamma=0.99, K_epochs=80, eps_clip=0.2, action_std_init=0.6)
-                        ppo_agent.train(random_seed=5, episodes=3000)
+                        ppo_agent.train(random_seed=4, episodes=3000)
 
     elif action == "3":
         algo = input("Choose the algorithm you want to use: \n[1] DQN \n[2] PPO \n")
